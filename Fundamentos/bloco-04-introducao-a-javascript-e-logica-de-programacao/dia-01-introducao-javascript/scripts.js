af39ef58830febdb74af4fcf1f263e36e728723d
Eric Vinícius <ericvinicius81@gmail.com>
2021-08-25 18:11:00 -0300

    Exercícios de introdução resolvidos

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/scripts.js
@@ -0,0 +1,17 @@
+const myName = "Eric";
+
+const birthCity = "Belo Horizonte";
+
+let birthYear = 1979;
+
+console.log(myName,birthCity,birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Salvador";
+
+console.log(birthCity);
+
+// Ocorreu um erro pois birthCity é uma constante
