eb547671ae66fdc76a962c43ee5b67b457f19243
Eric Vinícius <ericvinicius81@gmail.com>
2021-08-31 20:47:13 -0300

    Altera o arquivo README e adiciona exercícios resolvidos até o 4

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/scripts.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/scripts.js
@@ -1,17 +1,50 @@
-const myName = "Eric";
+// Exercício 1
+const a = 2;
+const b = 2;
 
-const birthCity = "Belo Horizonte";
+console.log(a + " + " + b + " é: " + (a + b));
+console.log(a + " - " + b + " é: " + (a - b));
+console.log(a + " x " + b + " é: " + (a * b));
+console.log(a + " / " + b + " é: " + (a / b));
+console.log(a + " % " + b + " é: " + (a % b));
 
-let birthYear = 1979;
+// Exercício 2
 
-console.log(myName,birthCity,birthYear);
+if (a > b) {
+  console.log(a + " > " + b);
+}
+else if (a < b) {
+  console.log(b + " > " + a);
+}
+else {
+  console.log(a + " = " + b);
+}
 
-birthYear = 2030;
+// Exercício 3
 
-console.log(birthYear);
+const c = 3;
+const d = 11;
+const e = 15;
 
-birthCity = "Salvador";
+if (c > d && c > e) {
+  console.log(c + " é o maior número");
+}
+else if (d > c && d > e) {
+  console.log(d + " é o maior número");
+}
+else {
+  console.log(e + " é o maior número");
+}
+ // Exercício 4
 
-console.log(birthCity);
+ const number = 5;
 
-// Ocorreu um erro pois birthCity é uma constante
+ if (number > 0) {
+   console.log("positive");
+ }
+ else if (number < 0) {
+   console.log("negative");
+ }
+ else {
+   console.log("zero");
+ }
