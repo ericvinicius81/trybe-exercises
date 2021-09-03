e01aff569d99a7471b759b39a609a2a246fad9f0
Eric Vinícius <ericvinicius81@gmail.com>
2021-09-03 08:56:32 -0300

    Adiciona os arquivos do dia 03, README.md e scripts.js, e exercícios parcialmente resolvidos

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,27 @@
+// Ex. 1
+
+let n = 3;
+let simbolo = '*';
+let linha = '';
+
+for (let index = 0; index < n; index += 1) {
+  linha += simbolo;
+};
+
+for (let index = 0; index < n; index += 1) {
+  console.log(linha);
+};
+
+// Ex. 2
+
+let base = 5;
+let simbolo = '*';
+let linha = '';
+
+for (let index = 0; index <= base; index += 1) {
+  console.log(linha);
+  linha += simbolo;
+};
+
+//Ex. 3
+
