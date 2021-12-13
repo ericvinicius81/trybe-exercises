fa8675284f40256a3d282b166ca02f9e90fabccc
Eric <ericvinicius81@yahoo.com.br>
2021-12-12 22:28:49 -0300

    Atualizaçao 7.1

--- Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,4 +1,25 @@
-const test = testingScope = (escopo) => {
+// Parte I
+
+// 1. Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// Modifique a estrutura da função para que ela seja uma arrow function .
+// Modifique as concatenações para template literals .
+// Copie o código abaixo.
+
+// function testingScope(escopo) {
+//   if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
+
+// testingScope(true);
+
+const testingScope = escopo => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
@@ -7,36 +28,55 @@ const test = testingScope = (escopo) => {
     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
-}
+  // console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+};
 
 testingScope(true);
 
+
+// 2. Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// // Seu código aqui.
+
+// console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-function sortedOddsAndEvens() {
-  oddsAndEvens[0] = 2;
-  oddsAndEvens[1] = 3;
-  oddsAndEvens[2] = 4;
-  oddsAndEvens[3] = 7;
-  oddsAndEvens[4] = 10;
-  oddsAndEvens[5] = 13;
+const sortNumbers = numbers => {
+  const sort = numbers.sort((a, b) => a - b);
+  return sort;
+};
+
+console.log(`Os números  ${sortNumbers(oddsAndEvens)} se encontram ordenados de forma crescente!`);
+
+
+// Parte II
+
+// Abaixo, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que aprendeu sobre let , const , arrow functions , template literals e ternary operator .
+
+// 1. Crie uma função que receba um número e retorne seu fatorial.
+
+// Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
+
+const fatorial = number => {
+  let value = 1;
+
+  for (let index = 1; index <= number; index += 1) {
+      value = value * index;
+  }
+  return value;
 }
 
-const newOddsAndEvens = oddsAndEvens.sortedOddsAndEvens(oddsAndEvens);
-console.log(newOddsAndEvens);
+console.log(fatorial(4));
 
 
-const sortOddsAndEvens = () => {
-  oddsAndEvens[0] = 2;
-  oddsAndEvens[1] = 3;
-  oddsAndEvens[2] = 4;
-  oddsAndEvens[3] = 7;
-  oddsAndEvens[4] = 10;
-  oddsAndEvens[5] = 13;
-
-  return sortOddsAndEvens;
-}
-
-console.log(sortOddsAndEvens);
-
- // será necessário alterar essa linha 😉
+// const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
+// console.log(factorial(3));
