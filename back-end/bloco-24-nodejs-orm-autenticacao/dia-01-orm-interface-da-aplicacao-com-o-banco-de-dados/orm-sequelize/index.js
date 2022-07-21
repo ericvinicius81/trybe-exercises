6b3984877d6a9aa096ca794fb7a333244e55251b
eric <ericvinicius81@yahoo.com.br>
2022-07-21 18:44:46 -0300

    cria a rota do tipo get /books/:id

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/orm-sequelize/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/orm-sequelize/index.js
@@ -7,5 +7,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/books', bookController.getAll);
+app.get('/books/:id', bookController.getById);
 
 app.listen(PORT, () => console.log(`O Pai tá ON na PORTA ${PORT}`));
