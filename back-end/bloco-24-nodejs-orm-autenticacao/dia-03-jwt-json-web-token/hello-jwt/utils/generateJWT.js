88d927c25e0548aefe9d33b8c0465f1ae4afd416
Eric Vinícius <ericvinicius81@yahoo.com.br>
2022-08-11 10:43:48 -0300

    feat: cria a rota /login com jwt

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-03-jwt-json-web-token/hello-jwt/utils/generateJWT.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const jwtConfig = {
+  expiresIn: '1h',
+  algorithm: 'HS256',
+};
+
+const generateJWT = (payload) => {
+  const token = jwt.sign({ data: payload }, process.env.JWT_SECRET, jwtConfig);
+
+  return token;
+};
+
+module.exports = generateJWT;
