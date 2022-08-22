552796bdb05f99018540be02e4c3c35c9f13a453
Eric Vinícius <ericvinicius81@yahoo.com.br>
2022-08-22 10:53:12 -0300

    chore: adiciona a model Plan

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-02-orm-associations/exercise-sequelize-associations/models/Plan.js
@@ -0,0 +1,26 @@
+const Plan = (sequelize, DataTypes) => {
+  const Plan = sequelize.define('Plan', {
+    plan_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    coverage: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.DOUBLE,
+    },
+  },
+  {
+    timestamps: false,
+    tablename: 'Plans',
+    underscored: true,
+  });
+
+  Plan.associate = (models) => {
+    Plan.hasMany(models.Patient, { foreignKey: 'plan.id', as: 'patients' });
+  };
+
+  return Plan;
+};
